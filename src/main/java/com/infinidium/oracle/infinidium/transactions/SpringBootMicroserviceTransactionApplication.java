ba6e8162dadc64e76ab0fde24e978fdfad0043f6
halukalp <haluk.allp@gmail.com>
2022-06-07 18:16:49 +0300

    initial commit

--- src/main/java/com/infinidium/oracle/infinidium/transactions/SpringBootMicroserviceTransactionApplication.java
+++ src/main/java/com/infinidium/oracle/infinidium/transactions/SpringBootMicroserviceTransactionApplication.java
@@ -3,8 +3,10 @@ package com.infinidium.oracle.infinidium.transactions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootMicroserviceTransactionApplication {
 
 	public static void main(String[] args)
